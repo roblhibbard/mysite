3a1633d3b86619c61ee0ad4423a159f6b3656e46
roblhibbard <robhlibbard@gmail.com>
2020-02-25 08:47:16 -0700

    initial committ add

---
+++ first_app/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Topic,Webpage,AccessRecord
+from . import forms
+
+# Create your views here.
+def index(request): #request is just a variable and does not matter
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {"access_records":webpages_list}
+    return render(request, 'first_app/index.html', context=date_dict)
+def form_name_view(request):
+    form = forms.FormName()
+
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+
+            print('VALIDATIONS SUCCESS!')
+            print("NAME:"+form.cleaned_data['name'])
+            print("EMAIL:" + form.cleaned_data['email'])
+            print("TEXT:" + form.cleaned_data['text'])
+
+    return render(request, 'first_app/form_page.html', {'form':form})
+
